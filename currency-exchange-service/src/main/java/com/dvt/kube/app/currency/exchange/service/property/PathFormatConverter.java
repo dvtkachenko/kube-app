e2658c6f77a43c2cd6364bbfa3b5750e34a19f80
Dmitriy Tkachenko <smartphone000014@gmail.com>
2020-03-01 14:36:10 +0200

    constants names changed

--- currency-exchange-service/src/main/java/com/dvt/kube/app/currency/exchange/service/property/PathFormatConverter.java
+++ currency-exchange-service/src/main/java/com/dvt/kube/app/currency/exchange/service/property/PathFormatConverter.java
@@ -2,21 +2,21 @@ package com.dvt.kube.app.currency.exchange.service.property;
 
 public class PathFormatConverter {
 
-    public static String URL_PREFIX = "file:";
-    public static String CLASSPATH_PREFIX = "classpath:";
+    public static String FILE_URL_PREFIX = "file:";
+    public static String CLASSPATH_URL_PREFIX = "classpath:";
 
     public String convertToResourceFitFormat(String sourcePath) {
         if(isUrlPathType(sourcePath) || isClassPathType(sourcePath)) {
             return sourcePath;
         }
-        return URL_PREFIX + sourcePath;
+        return FILE_URL_PREFIX + sourcePath;
     }
 
     private boolean isClassPathType(String checkedPath) {
-       return checkedPath.startsWith(CLASSPATH_PREFIX);
+       return checkedPath.startsWith(CLASSPATH_URL_PREFIX);
     }
 
     private boolean isUrlPathType(String checkedPath) {
-        return checkedPath.startsWith(URL_PREFIX);
+        return checkedPath.startsWith(FILE_URL_PREFIX);
     }
 }
